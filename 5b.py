b2d8910a0a09515f5be46c6ffb3eb3b76bdc0a9c
gogle7 <40571061+gogle7@users.noreply.github.com>
2019-11-12 20:22:25 +0530

    Update 5b.py

--- 5b.py
+++ 5b.py
@@ -11,10 +11,19 @@ else:
     print("Number is not present in fibonacci series.")
 
 i,flag=1,0
-while(i**2<=0):
+while(i**2<=n):
     if(i**2==n):
         flag=1
-    print("Perfect Square")
+        print("Perfect Square")
     i+=1
 if(flag==0):
     print("Not perfect square")
+
+i,flag=0,0
+while (2**i<=n):
+    if (2**i==n):
+        flag=1
+        print("Perfect power of 2")
+    i+=1
+if (flag==0):
+    print("Not Perfect power of 2")
